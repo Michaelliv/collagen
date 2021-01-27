ef0ca6f95292b18940fed30cebff2414287f9553
Michael Livshits <livsmichael@gmail.com>
2021-01-27 13:45:07 +0200

    Updated collagen entry

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import sys
+import argparse
 from pathlib import Path
-from typing import List
 
 from collagen.build_marketplace_docs import BuildMarketplaceDocs
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--source", help="source directory path")
+    parser.add_argument("-t", "--target", help="target directory path")
+    args = parser.parse_args()
 
-def main(argv: List[str]):
-    source = Path(argv[0]).absolute()
-    target = Path(argv[1]).absolute()
+    source = Path(args.source).absolute()
+    target = Path(args.target).absolute()
 
     assert source.exists()
     assert target.exists()
 
     build = BuildMarketplaceDocs(source=str(source), target=str(target))
     build.build()
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
